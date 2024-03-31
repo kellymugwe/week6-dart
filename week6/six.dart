ed5633ebbc41d91973bafd6f779e83c9421a1c6d
kellymugwe <114218602+kellymugwe@users.noreply.github.com>
2024-03-31 14:02:37 +0300

    Add files via upload

---
+++ week6/six.dart
@@ -0,0 +1,32 @@
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
+
+void main() {
+  String value = "example";
+
+  switch (value) {
+    case "example":
+      print("This is an example");
+      break;
+    case "test":
+      print("This is a test");
+      break;
+    default:
+      print("Unknown value");
+  }
+}
+
+void main() {
+  int i = 20;
+  while (i >= 10) {
+    print(i);
+    i--;
+  }
+}
